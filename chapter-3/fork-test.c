0a6cb01f0c3f550f688df0d8b8e8f0c6bd00203c
Sendy Halim <sendyhalim93@gmail.com>
2022-12-11 20:34:02 +0700

    feat(ch3): Add fork test

---
+++ chapter-3/fork-test.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+  pid_t pid = fork();
+
+  // Child process will always get pid of 0 from fork
+  // because it's kind of a marker.
+  printf("There should be 2 outputs of this log. PID: %d\n", pid);
+
+  if (pid < 0) {
+    fprintf(stderr, "Fork failed bro.. %d", pid);
+  } else if (pid == 0) {
+    execlp("/bin/ls", "ls", NULL);
+  } else {
+    wait(NULL);
+    printf("child complete");
+  }
+}
